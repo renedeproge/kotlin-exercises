c5869010227bd92512545e0a30aa76be4f58c58f
Spartan <unnamedspartan@caramail.com>
2024-12-16 10:29:39 +0100

    Exercise: Callback function wrappers - OK

--- src/main/kotlin/coroutines/suspension/FetchTasksUseCase.kt
+++ src/main/kotlin/coroutines/suspension/FetchTasksUseCase.kt
@@ -16,11 +16,30 @@ class FetchTasksUseCase(
 ) {
     @Throws(ApiException::class)
     suspend fun fetchTasks(): List<Task> =
-        TODO()
+        suspendCancellableCoroutine { continuation ->
+            val cancellable = callbackUseCase.fetchTasks(
+                onSuccess = { continuation.resume(it) },
+                onError = { continuation.resumeWithException(it) })
+            continuation.invokeOnCancellation { cancellable.cancel() }
+        }
+
+    // which should return Result representing either a success or a failure
     suspend fun fetchTasksResult(): Result<List<Task>> =
-        TODO()
+        suspendCancellableCoroutine { continuation ->
+            val cancellable = callbackUseCase.fetchTasks(
+                onSuccess = { continuation.resume(Result.success(it)) },
+                onError = { continuation.resume(Result.failure(it)) })
+            continuation.invokeOnCancellation { cancellable.cancel() }
+        }
+
+    // which should return tasks or null in the case of an error
     suspend fun fetchTasksOrNull(): List<Task>? =
-        TODO()
+        suspendCancellableCoroutine { continuation ->
+            val cancellable = callbackUseCase.fetchTasks(
+                onSuccess = { continuation.resume(it) },
+                onError = { continuation.resume(null) })
+            continuation.invokeOnCancellation { cancellable.cancel() }
+        }
 }
 
 interface FetchTasksCallbackUseCase {
